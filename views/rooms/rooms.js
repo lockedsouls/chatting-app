0fefba1a18f7d511dbbc4eb0c4bd50c87c998f56
lockedsouls <lockedssouls@gmail.com>
2022-12-01 20:57:40 +0200

    Implement room.js addChat()

--- views/rooms/rooms.js
+++ views/rooms/rooms.js
@@ -4,3 +4,27 @@ const socket = io("http://localhost:3000/rooms");
 socket.on("connect", () => {
     console.log(socket.id);
 });
+
+//room.js
+//functions
+function addChat(){
+    const chat = document.createElement("div"); chat.classList.add("chat-message");
+    const chat_sender = document.createElement("span");
+    chat_sender.classList.add("sender"); chat_sender.classList.add("local");
+    chat_sender.innerHTML = "ichavamp: ";
+
+    const chat_message = document.createElement("span");
+    chat_message.classList.add("message");
+    chat_message.innerHTML = "djskalwjdwlkadjalkwdj";
+    
+    chat.appendChild(chat_sender); chat.appendChild(chat_message);
+    document.querySelector("#chat").appendChild(chat);
+    return chat;
+}
+
+
+document.querySelector("#chat-box").addEventListener("keyup", event => {
+    if (event.key == "Enter"){
+        console.log(addChat(2));        
+    }
+})
